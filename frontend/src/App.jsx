360d98f2e9b723378f3448d6dba8828edd0bd36d
DumeGrisoni <misanthrope20218@gmail.com>
2023-01-21 09:56:58 +0100

    Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react'
+import {BrowserRouter, Link, Routes, Route} from 'react-router-dom';
+import {logo} from './assets/logo.svg';
+import {Home, CreateImage} from './pages';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-black sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <Link to='/'>
+          <img src={logo} alt="Logo de l'application" className='w-28 object-contain' /> 
+          
+        </Link>
+      </header>
+    </BrowserRouter>
   )
 }
 
